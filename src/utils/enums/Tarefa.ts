f7e9d69c9404fa717606610ae461bd88a2a1a1bb
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-06-19 23:47:57 -0300

    enums

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importante',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PEDENTE = 'pendente',
+  CONCLUIDA = 'concluída'
+}
