259b3d160c2db23382df01a04efc02892a5699a6
Guilherme Oliveira dos Santos <guilhermeolsnto@gmail.com>
2024-06-23 21:34:24 -0300

    rotas react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import { Provider } from 'react-redux'
-import BarraLateral from './Containers/BarraLateral'
-import ListaDeTarefas from './Containers/BarraLateral/ListaDeTarefas'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import EstiloGlobal, { Container } from './styles'
 
 import store from './store'
+import Home from './pages/home'
+import Cadastro from './pages/cadastro'
+
+const rotas = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/novo',
+    element: <Cadastro />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
       <Container>
-        <BarraLateral />
-        <ListaDeTarefas />
+        <RouterProvider router={rotas} />
       </Container>
     </Provider>
   )
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import BarraLateral from '../../Containers/BarraLateral'
+import ListaDeTarefas from '../../Containers/BarraLateral/ListaDeTarefas'
+import BotaoAdicionar from '../../components/botaoAdicionar'
+
+const Home = () => (
+  <>
+    <BarraLateral />
+    <ListaDeTarefas />
+    <BotaoAdicionar />
+  </>
+)
+
+export default Home
